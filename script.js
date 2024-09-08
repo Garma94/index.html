aa48636b4c85b55e026a5c4d48ea1a21dbcfd4cc
Garma94 <josegm3112@gmail.com>
2024-09-08 22:25:14 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('rsvp-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Gracias por confirmar tu asistencia!');
+    // Aquí podrías añadir código para enviar los datos a un servidor
+});
